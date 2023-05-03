4f14931b59e221952a859a97b4909860979f88ea
Lewis Pakoti <lewisnpakoti@gmail.com>
2023-05-03 23:12:12 +1200

    moved image folder. Showing on site. styling inline but will update later.

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,23 +1,32 @@
 import React from 'react'
 import "./home.scss"
+import img1 from '../../images/1.png'
+import socialIcon from '../../images/social_icon.png'
+import nzMapWhite from '../../images/nz_map_white.png'
 
 const Home = () => {
   return (
     // Page Div
     <div className="home">
 
-      {/* Left side of page */}
-      <div className="left">
-        <h1>Welcome!</h1>
-        <p>Congratulations on your pregnancy! Get started by finding a Māori Midwife near you today.</p>
-        <img src="../../assets/images/1.png" alt="imageOne" />
-        <p>Select your region</p>
-      </div>
+      {/* Left and right side container */}
+      <div className="home-container">
 
-      {/* Right side of page */}
-      <div className="right">
-        <img src="../../assets/images/nzMapWhite.png" alt="nzMapWhite" />
+        {/* Left side of page */}
+        <div className="left">
+          <h1>Welcome!</h1>
+          <p>Congratulations on your pregnancy!<br/> Get started by finding a Māori Midwife near you today.</p>
+          <div className="bullet-points">
+            <img src={img1} alt="imageOne" />
+            <img src={socialIcon} alt="social icon"/>
+            <p>Select your region</p>
+          </div>
+        </div>
 
+        {/* Right side of page */}
+        <div className="right">
+          <img src={nzMapWhite} width="100px" height="100px" alt="nzMapWhite" />
+        </div>
       </div>
     </div>
   )

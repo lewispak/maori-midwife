51c712822f1c745d167b1f707034280799918372
Lewis Pakoti <lewisnpakoti@gmail.com>
2023-05-08 17:27:54 +1200

    styled home page

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import "./home.scss"
 import img1 from '../../images/1.png'
-import socialIcon from '../../images/social_icon.png'
+import img2 from '../../images/2.png'
+import img3 from '../../images/3.png'
+import bg_img from '../../images/MD_wallpaper_1920x1080px.png'
+
 import nzMapWhite from '../../images/nz_map_white.png'
 
 const Home = () => {
@@ -16,16 +19,23 @@ const Home = () => {
         <div className="left">
           <h1>Welcome!</h1>
           <p>Congratulations on your pregnancy!<br/> Get started by finding a Māori Midwife near you today.</p>
-          <div className="bullet-points">
+          <div className="bullet-point">
             <img src={img1} alt="imageOne" />
-            <img src={socialIcon} alt="social icon"/>
             <p>Select your region</p>
+          </div>
+          <div className="bullet-point">
+            <img src={img2} alt="imageTwo" />
+            <p>View our midwives</p>
+          </div>
+          <div className="bullet-point">
+            <img src={img3} alt="imageThree" />
+            <p>Choose your midwife</p>
           </div>
         </div>
 
         {/* Right side of page */}
         <div className="right">
-          <img src={nzMapWhite} width="100px" height="100px" alt="nzMapWhite" />
+          <img src={nzMapWhite} width="350vw" height="500vh" alt="nzMapWhite" />
         </div>
       </div>
     </div>
